cb77aa896fe2f3dabd004763a948b255250356f6
Davide Bizzi <davide.bizzi@unguess.io>
2023-11-06 14:46:58 +0100

    feat: Add debounce

--- src/components/Sheet/Inputable/index.tsx
+++ src/components/Sheet/Inputable/index.tsx
@@ -1,21 +1,67 @@
-import { InputHTMLAttributes } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+
+class ChangeEvent extends Event {
+  constructor(value: string) {
+    super("change", { bubbles: true });
+    Object.defineProperty(this, "target", {
+      writable: false,
+      value: { value },
+    });
+  }
+}
 
 const InputableComponent = ({
   value,
   onChange,
   title,
   className,
+  type = "text",
 }: {
   value: string;
-  onChange: InputHTMLAttributes<HTMLInputElement>["onChange"];
+  onChange: (e: Event) => void;
   title?: string;
   className?: string;
+  type?: "text" | "textarea";
 }) => {
+  const [inputValue, setInputValue] = useState(value);
+  const [debouncedInputValue, setDebouncedInputValue] = useState(value);
+
+  useEffect(() => {
+    if (onChange && debouncedInputValue !== value) {
+      const event = new ChangeEvent(debouncedInputValue);
+      onChange(event);
+    }
+  }, [debouncedInputValue, value]);
+
+  useEffect(() => {
+    const delayInputTimeoutId = setTimeout(() => {
+      setDebouncedInputValue(inputValue);
+    }, 500);
+    return () => clearTimeout(delayInputTimeoutId);
+  }, [inputValue, 500]);
+
   return (
     <div className={className}>
       {title && <span>{title}</span>}
-      <input name={className} defaultValue={value} onChange={onChange} />
+      {type === "textarea" && (
+        <textarea
+          name={className}
+          defaultValue={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+      )}
+      {type === "text" && (
+        <input
+          name={className}
+          defaultValue={inputValue}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+      )}
     </div>
   );
 };
